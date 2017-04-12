d1d0a2490f55bbf1047bc9c3c6f4a36267fcb285
Miles <currymiles@gmail.com>
2017-04-12 11:03:38 -0700

    cacluates a decent SSE

--- assignment1/README.md
+++ assignment1/README.md
@@ -1,5 +1,14 @@
-# assignment2
+# assignment1
 - Miles Curry **(currymi)**
 - CS434
 - Dr. Xiaoli Fern
 - 9 April 2017
+
+
+# Results
+
+One dummy variable. 0 Random Variables added
+'''
+w =
+SSE =
+'''
--- assignment1/linearRegres.py
+++ assignment1/linearRegres.py
@@ -1,12 +1,18 @@
 import os.path as path
 import sys
-import numpy as np
+from numpy import arange, array, ones, linalg
+import numpy
 import re
 
-testFile = "./data/housing_test.txt"
-trainFile = "./data/housing_train.txt"
+testData = "./data/housing_test.txt"
+trainData = "./data/housing_train.txt"
+verbose = 0
 
-def load_data(file, dummy=1):
+"""
+Loads the data and returns a tuple with the first element being the
+X_i and b values and the second being the expected y values.
+"""
+def load_data(file, dummy=0):
     f = open(file, 'r')
     X = []
     Y = []
@@ -25,14 +31,45 @@ def load_data(file, dummy=1):
             for k in range(dummy):
                 X[i].insert(k, 1)
 
-    return X, Y
+    return X, Y # Returns a tuple. [0] = X_i values [1] = X_2 values
 
+"""
+Computes the optimal weight vector w.
+"""
+def train(data):
+    X = array(data[0])
+    Y = array(data[1])
+    return numpy.dot(linalg.inv(numpy.dot(X.T, X)), numpy.dot(X.T, Y))
 
+"""
+Test the optimal weights vector against the testing data
+"""
+def SSE(w, data, dummy=0):
+    X = array(data[0])
+    Y = array(data[1])
+    w = array(w)
 
+    i = 0; estimate = 0; sse = 0
+    for houses in data[0]:
+        estimate = numpy.sum(w[0:w.size].T * X[i][0:X[0].size])
+        sse += numpy.subtract(Y[i], estimate)**2
+        if(verbose > 0):
+            print "Estimated: {0} Actual: {1}".format(estimate, Y[i])
+        i += 1
+
+    return sse
 
 # Prevent running if imported as a module
 if __name__ == "__main__":
-    # Load from command line arguments or default files, otherwise exit
-    if path.isfile(testFile) and path.isfile(trainFile):
-        TRAIN = load_data(trainFile, dummy=1)
-        TEST = load_data(testFile)
+    if path.isfile(testData) and path.isfile(trainData):
+        TRAIN = load_data(trainData, dummy=1)
+        TEST = load_data(testData, dummy=1)
+    else:
+        print "Traning or Test File Not found!"
+        sys.quit(0)
+
+    print "Optimal weight vector w= "
+    w = train(TRAIN)
+    print w
+    sse = SSE(w, TEST)
+    print "SSE = ", sse
