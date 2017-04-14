1f36acc96bcb39783877d997b9f4ded1b379bb1d
Miles <currymiles@gmail.com>
2017-04-13 21:39:01 -0700

    updated with comments

--- assignment1/README.md
+++ assignment1/README.md
@@ -13,19 +13,6 @@ We then altered our learned weight vector by adding in dummy variables
 and by creating additional features at random to see the result of on
 the Sum of Square Error (SSE).
 
-# Results
-
-One dummy variable. 0 Random Variables added
-'''
-w = [  3.93620631e+01   4.27733574e-02   6.24991478e-03   3.14816354e+00
-  -1.68723350e+01   3.69754546e+00   8.09866767e-03  -1.53674556e+00
-   3.24131666e-01  -1.58107542e-02  -1.01573219e+00   1.00919488e-02
-  -6.19491108e-01]
-SSE = 1741.937
-'''
-
-
-
 # Running the Code
 The code runs a python 2 so it should be simple to run and requires pip
 to make sure you have the packages we use installed.
@@ -39,3 +26,16 @@ python linearRegres.py
 '''
 
 If you have any questions or problems, let us know. Thanks.
+
+# Results
+
+One dummy variable. 0 Random Variables added
+'''
+w = [  3.93620631e+01   4.27733574e-02   6.24991478e-03   3.14816354e+00
+  -1.68723350e+01   3.69754546e+00   8.09866767e-03  -1.53674556e+00
+   3.24131666e-01  -1.58107542e-02  -1.01573219e+00   1.00919488e-02
+  -6.19491108e-01]
+SSE = 1741.937
+'''
+
+%% TODO %%
--- assignment1/linearRegres.py
+++ assignment1/linearRegres.py
@@ -1,4 +1,5 @@
 import os.path as path
+import os
 import sys
 import argparse
 
@@ -10,11 +11,17 @@ testData = "./data/housing_test.txt"
 trainData = "./data/housing_train.txt"
 verbose = 0
 
+# TODO: Make the solution a class
+
 """
 Loads the data and returns a tuple with the first element being the
 X_i and b values and the second being the expected y values.
+
+file - the file to load the from
+dummy - the number of dummy variables to be added to the w vector
+random - the number of random variables to add to our training data
 """
-def load_data(file, dummy=0):
+def load_data(file, dummy=0, random=0):
     f = open(file, 'r')
     X = []
     Y = []
@@ -33,10 +40,14 @@ def load_data(file, dummy=0):
             for k in range(dummy):
                 X[i].insert(k, 1)
 
+    if(random > 0):
+        pass
+
     return X, Y # Returns a tuple. [0] = X_i values [1] = X_2 values
 
 """
 Computes the optimal weight vector w.
+data - the data we wish to get a w weight vector for
 """
 def train(data):
     X = array(data[0])
@@ -45,8 +56,10 @@ def train(data):
 
 """
 Test the optimal weights vector against the testing data
+w - optimal weight vector
+data - the data we wish to calculate an SSE for
 """
-def SSE(w, data, dummy=0):
+def SSE(w, data):
     X = array(data[0])
     Y = array(data[1])
     w = array(w)
@@ -64,6 +77,7 @@ def SSE(w, data, dummy=0):
 # Prevent running if imported as a module
 if __name__ == "__main__":
     """ Argument Parser """
+    # Pass -v to add verbose output
     parser = argparse.ArgumentParser(description='Homework Solution.')
     parser.add_argument("-v", '--verbose',
                         help='produces verbose output',
@@ -73,12 +87,37 @@ if __name__ == "__main__":
         verbose = 1
 
     """ Solution Start """
+
+    # Check for files
     if path.isfile(testData) and path.isfile(trainData):
-        TRAIN = load_data(trainData, dummy=1)
-        TEST = load_data(testData, dummy=1)
+        pass
     else:
         print "Traning or Test File Not found!"
         sys.quit(0)
 
-    print "Optimal weight vector w= "; w = train(TRAIN); print w
-    sse = SSE(w, TEST); print "SSE = ", sse
+
+    """ Dummy = 1 - No random variables added """
+    print "\n Dummy = 1 - No random Variables"
+    TRAIN = load_data(trainData, dummy=1)
+    TEST = load_data(testData, dummy=1)
+    print "Optimal weight vector w:"; w = train(TRAIN); print w
+    training_sse = SSE(w, TRAIN)
+    testing_sse = SSE(w, TEST)
+    print "Traning SSE = {0} \t Testing SSE = {1}\n".format(training_sse,
+                                                          testing_sse)
+
+    """ Dummy = 0 - No random variables added """
+    print "\nDummy = 0 - No random Variables"
+    TRAIN = load_data(trainData, dummy=0)
+    TEST = load_data(testData, dummy=0)
+    print "Optimal weight vector w: "; w = train(TRAIN); print w
+    training_sse = SSE(w, TRAIN)
+    testing_sse = SSE(w, TEST)
+    print "Traning SSE = {0} \t Testing SSE = {1}\n".format(training_sse,
+                                                          testing_sse)
+                                                          
+    # TODO: Random Variables added
+
+    # TODO: Variant of linear regression
+
+    # TODO: Regulation Terms
