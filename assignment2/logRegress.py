396886896596d44776e01014ba070635dd333794
Miles <currymiles@gmail.com>
2017-04-22 19:51:01 -0700

    added profiler

--- assignment2/logRegress.py
+++ assignment2/logRegress.py
@@ -41,32 +41,43 @@ class solution():
         for row in f:
             X.append(row[0:256])
             Y.append(row[256:257])
-            
+
         X = array(X); Y = array(Y)
         #X = X.astype(float64); Y = Y.astype(float64)
         X = np.longdouble(X); Y = np.longdouble(Y)
-        
+
         return X, Y # Returns a tuple of numpy arrays
+
+    def logloss(self, true_label, predicted, eps=1e-15):
+        p = np.clip(predicted, eps, 1 - eps)
+        if true_label == 1:
+            if p == 0:
+                p = 10**-10
+            return -np.log(p)
+        else:
+            if p == 1:
+                p = 1 - 10**-10
+            return -np.log(1 - p)
 
     def train(self, train_data, learning_rate, iterations):
         # Create W and D
         w = zeros(256)
         while(iterations > 0):
-            
+
             d = zeros(256)
             for i in range(train_data[0].shape[0]):
                 # Grab X and Y
                 x = train_data[0][i]; y = train_data[1][i]
-            
+
                 # Calculate our guess for y
                 dot_prod = np.dot(-w.T, x)
                 denominator = np.longdouble(1 + np.exp(dot_prod))
                 y_hat = np.longdouble(1/denominator)
-                
+
                 # Find Error and computer d
                 error = y - y_hat
                 d = d + (error * x)
-                
+
             # Update our W based on the learning rate
             w = w + (learning_rate * d)
             iterations -= 1
@@ -78,36 +89,27 @@ class solution():
         for i in range(test_data[0].shape[0]):
             # Grab X and Y
             x = train_data[0][i]; y = train_data[1][i]
-            
+
             # Calculate our guess for y
             dot_prod = np.dot(-w.T, x)
             denominator = np.longdouble(1 + np.exp(dot_prod))
             y_hat = np.longdouble(1/denominator)
-            
+
             # Calculate loss
-            loss += np.longdouble(logloss(y, y_hat))
+            loss += np.longdouble(self.logloss(y, y_hat))
 
         return loss
 
-        
+
+
 def frange(x, y, jump):
     while x < y:
         yield x
         x += jump
 
-        
-def logloss(true_label, predicted, eps=1e-15):
-    p = np.clip(predicted, eps, 1 - eps)
-    if true_label == 1:
-        if p == 0:
-            p = 10**-10
-        return -np.log(p)
-    else:
-        if p == 1:
-            p = 1 - 10**-10
-        return -np.log(1 - p)
-     
-     
+
+
+
 if __name__ == "__main__":
     """ Argument Parser """
     # Pass -v to add verbose output
@@ -125,26 +127,21 @@ if __name__ == "__main__":
 
     w = sol.train(train_data, 1, 175)
     print "W: ", w
-    
+
     loss = sol.test(w, test_data)
     print "Loss: ", loss
-    
+
     if verbose > 0:
         min = 1000
         min_learning_rate = 0
         for i in frange(0, 0.001, 0.0001):
-            
+
             loss = sol.test(sol.train(train_data,i,170), test_data)
             if loss <= min:
                 min = loss
                 min_learning_rate = i
-            
+
             print "i = {0} Loss: {1}".format(i, loss)
-        
+
         print "MIN_LOSS: ", min
         print "MIN_LEARNING_RATE: ", min_learning_rate
-        
-   
-
-   
-   
--- assignment2/profiler.py
+++ assignment2/profiler.py
@@ -4,4 +4,47 @@ import sys
 
 import matplotlib.pyplot as plt
 
-pass
+from logRegress import solution
+
+def graph_accurace(min_losses):
+    plt.cla()
+    plt.clf()
+
+    # Create figure
+    plt.title("Training Rate Accuracy")
+    plt.xlabel("x")
+    plt.ylabel("y")
+
+    plt.plot(days, observed_avg_temps, "r+")
+
+    plt.savefig("docs/training_accuracy.png")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Homework Solution.')
+    parser.add_argument("-v", '--verbose',
+                        help='produces verbose output')
+    args = parser.parse_args()
+
+    """ Solution Start """
+    sol = solution(trainData, testData)
+    train_data = sol.load_data(trainData)
+    test_data = sol.load_data(testData)
+
+    w = sol.train(train_data, 1, 175)
+
+    if verbose > 0:
+        min_losses = []
+        min = 1000
+        min_learning_rate = 0
+        for i in frange(0, 0.001, 0.0001):
+
+            loss = sol.test(sol.train(train_data,i,170), test_data)
+            if loss <= min:
+                min = loss
+                min_learning_rate = i
+
+            min_losses.append(loss)
+            print "i = {0} Loss: {1}".format(i, loss)
+
+        print "MIN_LOSS: ", min
+        print "MIN_LEARNING_RATE: ", min_learning_rate
