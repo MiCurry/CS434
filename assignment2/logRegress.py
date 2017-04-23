9a1223e9e8d308f19f25becf082fedf4ee2d145d
armatasc <armatasc@onid.oregonstate.edu>
2017-04-22 17:01:09 -0700

    Fixed the load_data function to grab all 256 data features, created a new loss function to avoid log(0), and tested numerous iteration and learning rate values to test for best combinations... --Christian

--- assignment2/logRegress.py
+++ assignment2/logRegress.py
@@ -2,6 +2,7 @@ import os.path as path
 import os
 import sys
 import argparse
+import math
 
 import csv
 from numpy import arange, array, ones, linalg, zeros
@@ -14,6 +15,7 @@ verbose = 0
 
 FOUR = 0
 NINE = 1
+np.seterr(over='ignore')
 
 class solution():
     def __init__(self, train_file, test_file):
@@ -37,36 +39,37 @@ class solution():
 
         f = csv.reader(f)
         for row in f:
-            X.append(row[0:255])
-            Y.append(row[256])
-
+            X.append(row[0:256])
+            Y.append(row[256:257])
+            
         X = array(X); Y = array(Y)
-        X = X.astype(np.float); Y = Y.astype(np.float)
-
+        #X = X.astype(float64); Y = Y.astype(float64)
+        X = np.longdouble(X); Y = np.longdouble(Y)
+        
         return X, Y # Returns a tuple of numpy arrays
 
     def train(self, train_data, learning_rate, iterations):
         # Create W and D
-        w = zeros(255)
+        w = zeros(256)
         while(iterations > 0):
-            iterations -= 1
-            d = zeros(255)
-
+            
+            d = zeros(256)
             for i in range(train_data[0].shape[0]):
                 # Grab X and Y
                 x = train_data[0][i]; y = train_data[1][i]
-
+            
                 # Calculate our guess for y
                 dot_prod = np.dot(-w.T, x)
-                y_hat = 1 / (1 + np.exp(dot_prod))
-
+                denominator = (1 + np.exp(dot_prod))
+                y_hat = 1/denominator
+                
                 # Find Error and computer d
                 error = y - y_hat
-                d = d + error * x
-
+                d = d + (error * x)
+                
             # Update our W based on the learning rate
-            #w = w + (learning_rate * d)
-            w = np.add(w, np.dot(learning_rate, d))
+            w = w + (learning_rate * d)
+            iterations -= 1
 
         return w
 
@@ -74,27 +77,37 @@ class solution():
         loss = 0
         for i in range(test_data[0].shape[0]):
             # Grab X and Y
-            x = test_data[0][i]; y = test_data[1][i]
-
-            dot_prod = np.dot((-w.T), x)
-            y_hat = 1 / ( 1 + (np.exp(dot_prod)))
-
-            print "E: {0} A: {1}\t".format(y_hat, y),
-
-            if y == 1:
-                loss += -np.log(y_hat)
-            elif y == 0:
-                loss += -np.log(1 - y_hat)
-            else:
-                print "Error: Should not have gotten here!"
+            x = train_data[0][i]; y = train_data[1][i]
+            
+            # Calculate our guess for y
+            dot_prod = np.dot(-w.T, x)
+            denominator = np.longdouble(1 + np.exp(dot_prod))
+            y_hat = np.longdouble(1/denominator)
+            
+            # Calculate loss
+            loss += np.longdouble(logloss(y, y_hat))
 
         return loss
 
+        
 def frange(x, y, jump):
     while x < y:
         yield x
         x += jump
 
+        
+def logloss(true_label, predicted, eps=1e-15):
+    p = np.clip(predicted, eps, 1 - eps)
+    if true_label == 1:
+        if p == 0:
+            p = 10**-10
+        return -np.log(p)
+    else:
+        if p == 1:
+            p = 1 - 10**-10
+        return -np.log(1 - p)
+     
+     
 if __name__ == "__main__":
     """ Argument Parser """
     # Pass -v to add verbose output
@@ -104,16 +117,34 @@ if __name__ == "__main__":
     args = parser.parse_args()
     if args.verbose > 0:
         verbose = args.verbose
-        print verbose
 
     """ Solution Start """
     sol = solution(trainData, testData)
     train_data = sol.load_data(trainData)
     test_data = sol.load_data(testData)
 
+    w = sol.train(train_data, 1, 175)
+    print "W: ", w
+    
+    loss = sol.test(w, test_data)
+    print "Loss: ", loss
+    
     if verbose > 0:
-        for i in frange(-2, 2, 0.5):
-            print "i = {0} Loss: {1}".format(i, \
-            sol.test(sol.train(train_data,10,25), test_data))
+        min = 1000
+        min_learning_rate = 0
+        for i in frange(0, 0.001, 0.0001):
+            
+            loss = sol.test(sol.train(train_data,i,170), test_data)
+            if loss <= min:
+                min = loss
+                min_learning_rate = i
+            
+            print "i = {0} Loss: {1}".format(i, loss)
+        
+        print "MIN_LOSS: ", min
+        print "MIN_LEARNING_RATE: ", min_learning_rate
+        
+   
 
-    w = sol.train(train_data, i, 3)
+   
+   
