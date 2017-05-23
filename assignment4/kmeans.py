58e0be5d36bb81c273d5286bb08402edc8525c4b
Miles Curry <currymi@oregonstate.edu>
2017-05-22 18:06:03 -0700

    working branch

--- assignment4/kmeans.py
+++ assignment4/kmeans.py
@@ -73,6 +73,77 @@ def kmeans(data, k, epochs=1):
 
     return sse
 
+"""
+I want to sent out a quick note about efficiency of the HAC algorithms. Complete
+and single link algorithms have a run time of O(n^2). If you algorithm is taking
+excessively long time, check the following.
+
+1. From one iteration to the next, any pairwise distance between two clusters
+that have not been changed in last iteration  does need to be recomputed.
+
+2. To compute the distance between a newly merged cluster and a previous cluster
+can be done in constant time. Say you have merged cluster A and B. To compute
+the distance between the merged AB with another cluster C, you just need to take
+previous D(A, C) and D(B, C) and take the max for complete link and min for
+single link.
+
+"""
+
+"""
+Start with all objects in their own cluster
+Repeat until there is only one cluster
+    Among the current clusters, determine the two
+        clusters, ci, and cj, that are closests
+    Replace ci and cj with a single cluster ci U cj
+
+"""
+
+def hac(data):
+    clusters = []
+    cluster_min = []
+    distances = zeros((data.shape[0], data.shape[0]))
+    distances.fill(np.inf)
+
+    for i in range(data.shape[0]):
+        clusters.append([i]) # Each data is its own cluster
+
+    print len(clusters)
+    print len(clusters[0])
+
+    # Calculate the distances between each cluster
+    for i in range(len(clusters)):
+        for j in range(i+1, len(clusters)):
+            distances[i][j] = np.linalg.norm(data[i] - data[j])
+
+    while(clusters != 10):
+        min_dist = np.inf
+        cj = 0
+        ci = 0
+
+        # Find the cluster with the minimum distance 
+        ci, cj = np.unravel_index(distances.argmin(), distances.shape)
+        print ci, cj
+                    
+        # Merge ci and cj
+        ## Create the new Cluster
+        clusters.append(clusters[ci] + clusters[cj])
+        distances[ci][cj] = np.inf
+
+        del clusters[ci]; del clusters[cj]
+
+        print ci, cj, len(clusters), distances.shape
+    
+
+    return 1
+
+    """
+    while len(clusters) != 10:
+        # Calculate or lookup Data Distance between each Cluster 
+        for i in range(len(clusters)):
+            for j in range(len(clusters))
+    """
+
+
 if __name__ == "__main__":
     """ Argument Parser """
     parser = argparse.ArgumentParser(description='Homework Solution.')
@@ -94,12 +165,14 @@ if __name__ == "__main__":
 
     """ Solution Start """
     print "Start"
-    data = load_data(dataFile)
+    data = load_data(smallData)
+    hac(data)
     
+
+    """
     sse = kmeans(data, 2, 10)
     print sse
     plot_sse(sse, 2)
-
     plt.subplot(111)
     sses = []
     for i in range(1, 11):
@@ -116,3 +189,4 @@ if __name__ == "__main__":
     plt.savefig("./docs/sse.png")
     print sses
     print min(sses)
+    """
