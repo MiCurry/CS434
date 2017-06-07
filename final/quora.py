a8451219ad68b8814d5fbc1e7bf9f5dcdd9cf406
Miles <currymiles@gmail.com>
2017-06-07 11:26:20 -0700

    updated with final folder

---
+++ final/quora.py
@@ -0,0 +1,37 @@
+import os.path as path
+import os
+import sys
+import argparse
+import math
+from random import randint
+
+import csv
+from numpy import arange, array, ones, linalg, zeros
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import pylab as plt
+
+from shared import load_data
+
+verbose = 0
+
+trainFile = "./data/quora-train.csv"
+testFile  = "./data/quora-test.csv"
+
+
+if __name__ == "__main__":
+    """ Argument Parser """
+    parser = argparse.ArgumentParser(description='Homework Solution.')
+    parser.add_argument("-v", '--verbose',
+                        help='produces verbose output')
+    args = parser.parse_args()
+    if args.verbose > 0:
+        verbose = args.verbose
+
+    np.set_printoptions(suppress=True)
+
+    """ Solution Start """
+    train_data = load_data(trainFile)
+    test_data = load_data(testFile)
+    print train_data
